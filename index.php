c525689b3a2ac3d909cc4db09ba24c155639f1b7
RSzyh <750332908@qq.com>
2018-05-07 21:53:03 +0800

    separate css

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ function getRecommend($conn) {
 <head>
     <meta charset="utf-8">
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="css/global.css">
     <title>ggNews</title>
 </head>
 <body>
@@ -115,98 +116,4 @@ function getRecommend($conn) {
         return HTML;
     }
 </script>
-<style>
-    .logo {
-        font-size: 24px;
-    }
-    .container {
-        display: block;
-        width: 1080px;
-        min-height: 1500px;
-    }
-    .sidebar {
-        float: left;
-        width: 110px;
-        margin-right: 50px;
-        text-align: center;
-    }
-    .sidebar ul {
-        padding: 0;
-    }
-    .sidebar .tab {
-        list-style-type: none;
-        display: inline-block;
-        width: 110px;
-        height: 40px;
-        line-height: 40px;
-        border-radius: 5px;
-        margin-bottom: 5px;
-        transition: background 200ms, color 200ms;
-    }
-    .sidebar .tab:hover, .sidebar .active {
-        background: #3cafe9;
-        cursor: pointer;
-        color: #ffffff;
-    }
-    .content {
-        float: left;
-        width: 660px;
-        height: 100%;
-    }
-    .search {
-        text-align: center;
-    }
-    .search input {
-        width: 500px;
-        height: 26px;
-        line-height: 26px;
-        padding: 0;
-        font-size: 16px;
-        outline: 0 none;
-    }
-    .search button {
-        width: 80px;
-        height: 28px;
-        line-height: 28px;
-        color: #3cafe9;
-        background-color: #ffffff;
-        box-shadow: 0px 0px 2px 2px #62bfee;
-        border: 0;
-        border-radius: 2px;
-        outline: 0 none;
-        transition: background-color 120ms, color 120ms, box-shadow 120ms;
-    }
-    .search button:hover {
-        cursor: pointer;
-        color: #ffffff;
-        background-color: #3cafe9;
-    }
-    .carousel {
-        margin-top: 30px;
-    }
-    .carousel img {
-        width: 660px;
-        height: 320px;
-    }
-    .item .title {
-        font-size: 18px;
-        font-weight: bold;
-        margin-bottom: 12px;
-        transition: color 200ms;
-    }
-    .item .title:hover {
-        cursor: pointer;
-        color: #3081aa;
-    }
-    .item .info span {
-        margin-right: 6px;
-        color: gray;
-    }
-    .item hr {
-        border: 0;
-        height: 0;
-        border-top: 1px solid rgba(0, 0, 0, 0.1);
-        border-bottom: 1px solid rgba(255, 255, 255, 0.3);
-    }
-</style>
 </html>
